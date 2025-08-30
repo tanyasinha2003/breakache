a8c66fd5949d5b3f627e8fae6c998c215d91242b
tanyasinha2003 <tanyasinha2003@gmail.com>
2025-08-30 16:24:07 +0530

    favicon added

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,15 @@
 import '../styles/global.css';
-
+import Head from "next/head";
 
 export default function App({ Component, pageProps }) {
-    return <Component {...pageProps} />;
+   return (
+    <>
+      <Head>
+        <link rel="icon" href="/favicon.png" />
+        <title>Breakache</title>
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
   }
   
